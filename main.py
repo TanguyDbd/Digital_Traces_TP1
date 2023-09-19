2e26ffc0f26c46029580f697da5bf6ebfdbe5ef0
TanguyDbd <tanguy.dabadie@epfedu.fr>
2023-09-19 12:36:14 +0200

    Google analytics2

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
-<script async
-src="https://www.googletagmanager.com/gtag/js?id=UA-285801678-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-85ECQBE1B7"></script>
 <script>
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', 'UA-285801678-1');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-85ECQBE1B7');
 </script>
  """
  return prefix_google + "Hello World"
