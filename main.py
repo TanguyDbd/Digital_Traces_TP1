b504f01ec10756bcc5edf8a4381ac3c30f85792a
TanguyDbd <tanguy.dabadie@epfedu.fr>
2023-09-19 13:10:24 +0200

    Final commit

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-85ECQBE1B7"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PBJEG6DKLE"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-85ECQBE1B7');
+  gtag('config', 'G-PBJEG6DKLE');
 </script>
  """
  return prefix_google + "Hello World"
