4072964534beb0bc6845b38a28a157e45413c256
TanguyDbd <tanguy.dabadie@epfedu.fr>
2023-09-19 12:31:03 +0200

    Google analytics

--- main.py
+++ main.py
@@ -3,5 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-    return "Hello from Space! 🚀"
+# def root():
+#     return "Hello from Space! 🚀"
+
+def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-285801678-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'UA-285801678-1');
+</script>
+ """
+ return prefix_google + "Hello World"
