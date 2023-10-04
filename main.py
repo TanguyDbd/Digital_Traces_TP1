87f4918fba6bbff0ff8e102a67ea449a2945c117
TanguyDbd <tanguy.dabadie@epfedu.fr>
2023-10-04 11:07:43 +0200

    logger messages

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-# def root():
-#     return "Hello from Space! 🚀"
-
-def hello_world():
- prefix_google = """
+prefix_google = """
  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PBJEG6DKLE"></script>
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-GV4KJPM75M"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-PBJEG6DKLE');
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'G-GV4KJPM75M');
 </script>
  """
- return prefix_google + "Hello World"
+
+@app.route("/")
+def hello_world():
+ return prefix_google + "Hello World main page"
+
+@app.route("/logger")
+def log():
+    # Print a message in Python
+    log_msg = "Vous êtes bien connectés à la page des logs"
+    app.logger.info(log_msg)
+
+    # Print a message in the browser console
+    browser_log = """
+    <script>
+        console.log('Console du web browser : Vous êtes bien connectés à la page des logs');
+    </script>
+    """
+    # The printed msg in python
+    print(log_msg)
+
+    return log_msg + browser_log
